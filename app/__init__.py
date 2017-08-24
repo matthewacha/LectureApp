cb6632f6ea318f89f346ef05d2a74792715c3eff
All Eyes <matthewacha@gmail.com>
2017-08-24 20:44:08 +0300

    edit __nit__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
 loginmanager=LoginManager()
 
 app=Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
+
 loginmanager.init_app(app)
 loginmanager.loginm_message = "You need to be logged in to view this page"
 loginmanager.login_view = "auth_login"
 
-from app import views
+
+from app import views,models

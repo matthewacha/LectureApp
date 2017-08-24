51e9b0068ed888d520a4ff5b0143bc43fcd43094
All Eyes <matthewacha@gmail.com>
2017-08-24 18:31:06 +0300

    edit init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_login import LoginManager
+loginmanager=LoginManager()
 
 app=Flask(__name__)
 app.config.from_object('config')
+loginmanager.init_app(app)
+loginmanager.loginm_message = "You need to be logged in to view this page"
+loginmanager.login_view = "auth_login"
 
-frpm app import views
+from app import views
